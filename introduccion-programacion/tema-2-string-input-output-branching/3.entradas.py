09969c7f1d44ef9131c45ace1f76dde5866f29b1
gianzk <g.gutierrez@uni.pe>
2025-04-01 07:41:40 -0500

    update

---
+++ introduccion-programacion/tema-2-string-input-output-branching/3.entradas.py
@@ -0,0 +1,3 @@
+entrada=input("ingrese el año")
+entradaNueva="este es el año ingresado "+entrada
+print(entradaNueva)
---
+++ introduccion-programacion/tema-2-string-input-output-branching/5.f-string.py
@@ -0,0 +1,7 @@
+# 3.6  python 
+print("nueva forma") # nueva forma
+valor=20
+mensage="el valor que tengo es"+ " "+ str(valor)
+print(mensage)
+print(f"el valor que tengo es {valor}")
+
